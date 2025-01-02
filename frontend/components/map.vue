d64e871f2d1ee5ea25305c66582eed22f823fcec
Thomas Raynaud <raynaud.thomas@gmail.com>
2025-01-02 19:20:46 +0100

    add canvas on top of map

--- frontend/components/map.vue
+++ frontend/components/map.vue
@@ -38,19 +38,40 @@
     const prev_y = ref(0)
 
     const map_url = ref([])
+    const map_container = ref(null)
+    const map_display = ref(null)
     const map_canvas = ref(null)
 
     const loading_tile = ref(false)
     
     onMounted(() => {
-        map_canvas.value.style["width"] = (TILE_SIZE * TILES_X) + "px"
-        map_canvas.value.style["height"] = (TILE_SIZE * TILES_Y) + "px"
+        let width = (TILE_SIZE * TILES_X)
+        let height = (TILE_SIZE * TILES_Y)
+        let width_str = width + "px"
+        let height_str = height + "px"
+        map_container.value.style["width"] = width_str
+        map_container.value.style["height"] = height_str
+        map_display.value.style["width"] = width_str
+        map_display.value.style["height"] = height_str
+        map_canvas.value.style["width"] = width_str
+        map_canvas.value.style["height"] = height_str
+        map_canvas.value.width = width
+        map_canvas.value.height = height
         let template_columns = ""
         for (let i = 0; i < TILES_X + 1; i++) {
             template_columns += "auto "
         }
-        map_canvas.value.style["grid-template-columns"] = template_columns
+        map_display.value.style["grid-template-columns"] = template_columns
         load_map()
+
+        let ctx = map_canvas.value.getContext("2d")
+        console.log(ctx)
+        ctx.beginPath()
+        ctx.moveTo(1, 1)
+        ctx.lineTo(300, 300)
+        ctx.strokeStyle = 'black'
+        ctx.lineWidth = '2'
+        ctx.stroke()
     })
 
     const get_tile = (x, y, z) => {
@@ -119,8 +140,8 @@
 
     const get_mouse_pos = (e) => {
         return {
-            x: e.clientX + document.documentElement.scrollLeft - map_canvas.value.offsetLeft,
-            y: e.clientY + document.documentElement.scrollTop - map_canvas.value.offsetTop
+            x: e.clientX + document.documentElement.scrollLeft - map_container.value.offsetLeft,
+            y: e.clientY + document.documentElement.scrollTop - map_container.value.offsetTop
         }
     }
 
@@ -135,9 +156,9 @@
     }
 
     const pan = (e) => {
-        const canvas = map_canvas.value
-        let scroll_x = canvas.scrollLeft + (prev_x.value - e.clientX)
-        let scroll_y = canvas.scrollTop + (prev_y.value - e.clientY)
+        const display = map_display.value
+        let scroll_x = display.scrollLeft + (prev_x.value - e.clientX)
+        let scroll_y = display.scrollTop + (prev_y.value - e.clientY)
         let max_ind = Math.pow(2, coords.z)
         let shift_x = 0
         let shift_y = 0
@@ -210,8 +231,8 @@
             }
             scroll_y -= TILE_SIZE
         }
-        canvas.scrollLeft = Math.min(scroll_x, TILE_SIZE)
-        canvas.scrollTop = Math.min(scroll_y, TILE_SIZE)
+        display.scrollLeft = Math.min(scroll_x, TILE_SIZE)
+        display.scrollTop = Math.min(scroll_y, TILE_SIZE)
         prev_x.value = e.clientX
         prev_y.value = e.clientY
     }
@@ -219,8 +240,8 @@
     const get_map_coords = (e) => {
         let mouse_pos = get_mouse_pos(e)
         return {
-            x: (Math.floor(mouse_pos.x / TILE_SIZE) + (coords.x + 1) + (((mouse_pos.x % TILE_SIZE) - (TILE_SIZE - map_canvas.value.scrollLeft)) / TILE_SIZE)) / Math.pow(2, coords.z),
-            y: (Math.floor(mouse_pos.y / TILE_SIZE) + (coords.y + 1) + (((mouse_pos.y % TILE_SIZE) - (TILE_SIZE - map_canvas.value.scrollTop)) / TILE_SIZE)) / Math.pow(2, coords.z)
+            x: (Math.floor(mouse_pos.x / TILE_SIZE) + (coords.x + 1) + (((mouse_pos.x % TILE_SIZE) - (TILE_SIZE - map_display.value.scrollLeft)) / TILE_SIZE)) / Math.pow(2, coords.z),
+            y: (Math.floor(mouse_pos.y / TILE_SIZE) + (coords.y + 1) + (((mouse_pos.y % TILE_SIZE) - (TILE_SIZE - map_display.value.scrollTop)) / TILE_SIZE)) / Math.pow(2, coords.z)
         }
     }
 
@@ -242,7 +263,7 @@
         let e_pos_left = Math.floor(pos_left)
         let dec_pos_left = pos_left - e_pos_left
         coords.x = e_pos_left
-        map_canvas.value.scrollLeft = dec_pos_left * TILE_SIZE
+        map_display.value.scrollLeft = dec_pos_left * TILE_SIZE
         // Y
         let rel_v_y = mouse_pos.y / (TILES_Y * TILE_SIZE)
         let pos_y = rel_point.y * nb_rows
@@ -250,11 +271,11 @@
         let e_pos_top = Math.floor(pos_top)
         let dec_pos_top = pos_top - e_pos_top
         coords.y = e_pos_top
-        map_canvas.value.scrollTop = dec_pos_top * TILE_SIZE
+        map_display.value.scrollTop = dec_pos_top * TILE_SIZE
         coords.z = z
         if (coords.z == 1) {
-            map_canvas.value.scrollLeft = 0
-            map_canvas.value.scrollTop = 0
+            map_display.value.scrollLeft = 0
+            map_display.value.scrollTop = 0
         }
         load_map()
     }
@@ -284,29 +305,41 @@
 </script>
 
 <style scoped>
-    #map-canvas {
-        overflow:hidden;
-        display: grid;
+    #map-container {
+        position: relative;
         padding: 0;
         margin: 0 10px;
     }
 
-    #map-canvas img {
+    #map-display {
+        position: absolute;
+        overflow:hidden;
+        display: grid;
+    }
+
+    #map-display img {
         width: 256px;
         height: 256px;
+    }
+
+    #map-canvas {
+        position: absolute;
+        z-index: 1;
     }
 </style>
 
 <template>
     <div class="body row">
-        <div id="map-canvas" ref="map_canvas"
-            @mousemove="map_mousemove"
-            @mouseup="map_selected=false"
-            @mousedown="map_mousedown"
-            @mouseleave="map_selected=false"
-            @wheel="zoom"
-        >
-            <img v-for="ind in (TILES_X + 1) * (TILES_Y + 1)" :src="map_url[ind - 1]">
+        <div id="map-container" ref="map_container"
+                @mousemove="map_mousemove"
+                @mouseup="map_selected=false"
+                @mousedown="map_mousedown"
+                @mouseleave="map_selected=false"
+                @wheel="zoom">
+            <div id="map-display" ref="map_display">
+                <img v-for="ind in (TILES_X + 1) * (TILES_Y + 1)" :src="map_url[ind - 1]">
+            </div>
+            <canvas id="map-canvas" ref="map_canvas"></canvas>
         </div>
         <p>{{ "x= " + cursor_coords.x + " y= " + cursor_coords.y + " z= " + coords.z }}</p>
         <p>{{ "mx= " + merc_cords.x}}</p>
